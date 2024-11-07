09f03dafdf959d302979ef220eb3fd079e0f0720
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-07 07:38:11 +0000

    feature: adds auth layout route path in routes definition

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './core/layouts/auth-layout/auth-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth',
+        component: AuthLayoutComponent,
+        children: []
+    },
+];
