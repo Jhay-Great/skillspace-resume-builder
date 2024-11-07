8b8b179ab0aab0daeec001689a136c5cdbed918c
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-07 07:37:11 +0000

    feature: adds auth layout markup and styles

--- src/app/core/layouts/auth-layout/auth-layout.component.ts
+++ src/app/core/layouts/auth-layout/auth-layout.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet, } from '@angular/router';
+
 
 @Component({
   selector: 'app-auth-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.scss'
 })
