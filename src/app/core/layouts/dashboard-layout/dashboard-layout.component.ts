e17175360f9aa7e0296898bc957d95bb5cc7edf1
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-07 11:16:16 +0000

    feature: adds more styles and markup to the dashboard layout

--- src/app/core/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/core/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink, ],
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.scss'
 })
