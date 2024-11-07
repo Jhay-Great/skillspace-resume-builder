8848f385aa918cec06d062fe7a497867816a759e
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-07 20:25:29 +0000

    feat: add reactive form
    Use form builder module to create company registration form and adds
    markup and style. Also adds methods for navigation stepper form

--- src/app/feature/user-registration/company-registration/company-registration.component.ts
+++ src/app/feature/user-registration/company-registration/company-registration.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { InputIconModule } from 'primeng/inputicon';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-company-registration',
   standalone: true,
-  imports: [ReactiveFormsModule, ],
+  imports: [ReactiveFormsModule, RouterLink, InputIconModule, IconFieldModule, InputTextModule, FormsModule, NgClass],
   templateUrl: './company-registration.component.html',
   styleUrl: './company-registration.component.scss'
 })
 export class CompanyRegistrationComponent implements OnInit {
   companyForm!:FormGroup;
+  step:number = 1;
 
   constructor (
     private fb: FormBuilder,
@@ -17,11 +24,32 @@ export class CompanyRegistrationComponent implements OnInit {
 
   ngOnInit(): void {
     this.companyForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-      confirmPassword: [''],
+      credentials: this.fb.group({
+        name: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.min(8)]],
+        confirmPassword: ['', [Validators.required]],
+      }),
+      information: this.fb.group({
+        website: ['', Validators.required],
+        certificate: ['', Validators.required],
+        logo: '',
+        contact: ['', Validators.required],
+      })
     })
+  }
+  
+  onSubmit() {
+    const formData = this.companyForm.value;
+    console.log(formData)
+  }
+
+  onContinue(step = 2) {
+    this.step = step;
+  }
+
+  onBack() {
+    this.step = this.step - 1;
   }
 
 }
