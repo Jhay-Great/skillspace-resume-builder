f25e4f1fe69687a789abcb9f4f99aa37d2118fe1
kewablay <kojokewajunior123@gmail.com>
2024-11-08 16:22:55 +0000

    feat(auth): add login and forgot password routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,32 @@ import { Routes } from '@angular/router';
 import { AuthLayoutComponent } from './core/layouts/auth-layout/auth-layout.component';
 
 export const routes: Routes = [
-    {
-        path: 'auth',
-        component: AuthLayoutComponent,
-        children: []
-    },
-    {
-        path: 'dashboard',
-        loadComponent: () => import('./core/layouts/dashboard-layout/dashboard-layout.component').then(d => d.DashboardLayoutComponent),
-        children: [],
-    },
+  {
+    path: 'auth',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./feature/authentication/pages/login/login.component').then(
+            (d) => d.LoginComponent
+          ),
+      },
+      {
+        path: 'forgot-password',
+        loadComponent: () =>
+          import(
+            './feature/authentication/pages/forgot-password/forgot-password.component'
+          ).then((d) => d.ForgotPasswordComponent),
+      },
+    ],
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./core/layouts/dashboard-layout/dashboard-layout.component').then(
+        (d) => d.DashboardLayoutComponent
+      ),
+    children: [],
+  },
 ];
