6bf695b1f46ea1cfa141cdd3b77836e0f57e6696
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-10 13:17:43 +0000

    feat: add markup and properties for reusable form input

--- src/app/shared/components/input-field/input-field.component.ts
+++ src/app/shared/components/input-field/input-field.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-field',
@@ -7,6 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './input-field.component.html',
   styleUrl: './input-field.component.scss'
 })
-export class InputFieldComponent {
+export class InputFieldComponent implements OnInit {
+  @Input ({required: true}) imagePath:string | null = null;
+  @Input ({required: true}) label!:string;
+  @Input ({required: true}) type:string | null = null;
+  @Input ({required: true}) placeholder:string | null = null;
+  @Input () controlName:string | null = null;
+  @Input () hasError:boolean = false;
+  @Input () errorMessage:string | null = null;
+
+  constructor () {}
+
+  ngOnInit() {
+    console.log({label: this.label, imagePath: this.imagePath, type:this.type})
+
+  }
 
 }
