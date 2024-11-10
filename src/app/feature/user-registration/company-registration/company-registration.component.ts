5e41e9d5f45ff48cb042dbd3614993ece9e35a68
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-10 13:09:31 +0000

    feat: builds company form
    Use form builder and reactive forms module to build reactive form.
    Adds form markup as well as reactive form module bindings.
    Also includes error handling for the first step of the form.

--- src/app/feature/user-registration/company-registration/company-registration.component.ts
+++ src/app/feature/user-registration/company-registration/company-registration.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { NgClass } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { CommonModule, NgClass } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 import { InputIconModule } from 'primeng/inputicon';
@@ -12,6 +12,9 @@ import { FileUploadModule } from 'primeng/fileupload';
 import { MessageService } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
 
+import { passwordStrengthValidator, confirmPasswordValidator } from '../../../shared/utils/password.validator';
+import { InputFieldComponent } from "../../../shared/components/input-field/input-field.component";
+
 interface UploadEvent {
     originalEvent: Event;
     files: File[];
@@ -20,7 +23,7 @@ interface UploadEvent {
 @Component({
   selector: 'app-company-registration',
   standalone: true,
-  imports: [FileUploadModule, ToastModule, ReactiveFormsModule, RouterLink, InputIconModule, IconFieldModule, InputTextModule, FormsModule, NgClass],
+  imports: [FileUploadModule, ToastModule, CommonModule, ReactiveFormsModule, RouterLink, InputIconModule, IconFieldModule, InputTextModule, FormsModule, NgClass, InputFieldComponent],
   providers: [MessageService],
   templateUrl: './company-registration.component.html',
   styleUrl: './company-registration.component.scss'
@@ -28,27 +31,16 @@ interface UploadEvent {
 export class CompanyRegistrationComponent implements OnInit {
   companyForm!:FormGroup;
   step:number = 1;
-
+  placeholder = 'enter your name'
+  label = 'something'
   constructor (
     private fb: FormBuilder,
     private messageService:MessageService,
   ) {};
 
   ngOnInit(): void {
-    this.companyForm = this.fb.group({
-      credentials: this.fb.group({
-        name: ['', Validators.required],
-        email: ['', [Validators.required, Validators.email]],
-        password: ['', [Validators.required, Validators.min(8)]],
-        confirmPassword: ['', [Validators.required]],
-      }),
-      information: this.fb.group({
-        website: ['', Validators.required],
-        certificate: ['', Validators.required],
-        logo: '',
-        contact: ['', Validators.required],
-      })
-    })
+    this.companyForm = this.formInitialization();
+
   }
   
   onSubmit() {
@@ -60,13 +52,41 @@ export class CompanyRegistrationComponent implements OnInit {
     this.step = step;
   }
 
+  getFormControl(controlName:string) {
+    return this.companyForm.get(controlName);
+  }
+
   onBack() {
     this.step = this.step - 1;
+  }
+
+  togglePasswordVisibility(inputElement: HTMLInputElement) {
+    console.log('called...')
+    inputElement.type = inputElement.type === 'password' ? 'text' : 'password';
+  }
+
+  private formInitialization () {
+    return this.fb.group({
+      credentials: this.fb.group({
+        name: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(8), passwordStrengthValidator()]],
+        confirmPassword: ['', [Validators.required]],
+      }),
+      information: this.fb.group({
+        website: ['', Validators.required],
+        certificate: ['', Validators.required],
+        logo: '',
+        contact: ['', Validators.required],
+      })
+    }, { validators: confirmPasswordValidator() })
   }
 
   onBasicUploadAuto(event: UploadEvent) {
     console.log(event);
     this.messageService.add({ severity: 'info', summary: 'Success', detail: 'File Uploaded with Auto Mode' });
 }
+
+
 
 }
