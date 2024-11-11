aa456f8d31552a417c6bc52b091b515e1e87b9b0
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-11 15:02:33 +0000

    style: reformat code for better readability

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,5 +8,10 @@ import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideStore(), provideEffects(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
+  providers: [
+    provideRouter(routes), 
+    provideAnimationsAsync(), 
+    provideStore(), provideEffects(), 
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+  ]
 };
