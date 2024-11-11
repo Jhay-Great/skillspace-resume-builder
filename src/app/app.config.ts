bd1898579e2db9a320899bc47b9060e17e31e7d6
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-11 16:13:21 +0000

    chore: add animation package to app config file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,12 +6,15 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideAnimationsAsync(), 
-    provideStore(), provideEffects(), 
+    provideAnimations(),
+    provideStore(), 
+    provideEffects(), 
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
   ]
 };
