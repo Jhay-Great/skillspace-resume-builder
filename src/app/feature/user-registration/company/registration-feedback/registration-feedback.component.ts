6220930a1b24c095eb2fde9936d6f3fbc9bd9107
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-12 07:37:05 +0000

    feat: add registration feedback component

---
+++ src/app/feature/user-registration/company/registration-feedback/registration-feedback.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-feedback',
+  standalone: true,
+  imports: [],
+  templateUrl: './registration-feedback.component.html',
+  styleUrl: './registration-feedback.component.scss'
+})
+export class RegistrationFeedbackComponent {
+
+}
