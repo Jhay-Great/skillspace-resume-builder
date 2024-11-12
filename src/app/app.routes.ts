b3e1ce4cabf0e1ee4a7e7c6ddb22324e9d14f6d1
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-12 19:15:50 +0000

    feat: add new company review page route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,20 +2,34 @@ import { Routes } from '@angular/router';
 import { AuthLayoutComponent } from './core/layouts/auth-layout/auth-layout.component';
 
 export const routes: Routes = [
-    {
-        path: 'auth',
-        component: AuthLayoutComponent,
-        children: [
-            {
-                path: 'company-registration',
-                loadComponent: () => import('./feature/user-registration/company/company-registration/company-registration.component').then(cr => cr.CompanyRegistrationComponent),
-                title: 'Company registration'
-            }
-        ]
-    },
-    {
-        path: 'dashboard',
-        loadComponent: () => import('./core/layouts/dashboard-layout/dashboard-layout.component').then(d => d.DashboardLayoutComponent),
-        children: [],
-    },
+  {
+    path: 'auth',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'company-registration',
+        loadComponent: () =>
+          import(
+            './feature/user-registration/company/company-registration/company-registration.component'
+          ).then((cr) => cr.CompanyRegistrationComponent),
+        title: 'Company registration',
+      },
+      {
+        path: 'review',
+        loadComponent: () =>
+          import(
+            './feature/user-registration/company/registration-feedback/registration-feedback.component'
+          ).then((rf) => rf.RegistrationFeedbackComponent),
+        title: 'Review Feedback',
+      },
+    ],
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./core/layouts/dashboard-layout/dashboard-layout.component').then(
+        (d) => d.DashboardLayoutComponent
+      ),
+    children: [],
+  },
 ];
