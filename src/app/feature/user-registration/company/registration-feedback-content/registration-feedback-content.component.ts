932c35bf62a9744a46240a0e08c79472f519dded
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-12 22:19:50 +0000

    feat: import registration feedback content in registration feedback component

--- src/app/feature/user-registration/company/registration-feedback-content/registration-feedback-content.component.ts
+++ src/app/feature/user-registration/company/registration-feedback-content/registration-feedback-content.component.ts
@@ -1,16 +1,16 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-registration-feedback',
+  selector: 'app-registration-feedback-content',
   standalone: true,
   imports: [],
   templateUrl: './registration-feedback-content.component.html',
   styleUrl: './registration-feedback-content.component.scss'
 })
 export class RegistrationFeedbackContentComponent {
+  @Input() svgImage:string | null = null;
   @Input() title:string | null = null;
   @Input() description:string | null = null;
-  @Input() activeButton:boolean = true;
-  @Input() buttonType: string | null = null;
+  @Input() buttonText: string | null = null;
 
 }
--- src/app/feature/user-registration/company/registration-feedback/registration-feedback.component.ts
+++ src/app/feature/user-registration/company/registration-feedback/registration-feedback.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { RegistrationFeedbackContentComponent } from '../registration-feedback-content/registration-feedback-content.component';
 
 @Component({
   selector: 'app-registration-feedback',
   standalone: true,
-  imports: [],
+  imports: [RegistrationFeedbackContentComponent],
   templateUrl: './registration-feedback.component.html',
   styleUrl: './registration-feedback.component.scss'
 })
 export class RegistrationFeedbackComponent {
+  isAwaitingReview:boolean = false;
+  isRejected:boolean = false;
+  isSuccessful:boolean = false;
 
 }
