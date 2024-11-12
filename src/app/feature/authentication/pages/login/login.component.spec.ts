3b72fec0a2784a41498a9acafb91559f74d4c4b9
kewablay <kojokewajunior123@gmail.com>
2024-11-12 16:30:14 +0000

    test(login): add tests for login component

--- src/app/feature/authentication/pages/login/login.component.spec.ts
+++ src/app/feature/authentication/pages/login/login.component.spec.ts
@@ -1,17 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { CapitalizePipe } from '../../../../core/pipes/capitalize/capitalize.pipe';
+  import { RouterLink } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [LoginComponent]
-    })
-    .compileComponents();
-    
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        LoginComponent,
+        ReactiveFormsModule,
+        IconFieldModule,
+        InputIconModule,
+        InputTextModule,
+        PasswordModule,
+        ButtonModule,
+        CheckboxModule,
+        CapitalizePipe,
+        RouterLink,
+        RouterTestingModule,
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +40,21 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a login form', () => {
+    expect(component.loginForm).toBeTruthy();
+  });
+
+  it('should validate email field', () => {
+    const emailControl = component.loginForm.get('email');
+    emailControl?.setValue('invalid-email');
+    expect(emailControl?.invalid).toBeTrue();
+  });
+
+  it('should validate password field', () => {
+    const passwordControl = component.loginForm.get('password');
+    passwordControl?.setValue('');
+    expect(passwordControl?.invalid).toBeTrue();
+  });
+
 });
