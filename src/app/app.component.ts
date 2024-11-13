393ea45b62e8c297c58631f1b061338dd97da64d
kewablay <kojokewajunior123@gmail.com>
2024-11-13 14:41:55 +0000

    feat: add toast to app.html and inject toast position from toast service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CustomToastComponent } from "./shared/components/custom-toast/custom-toast.component";
+import { ToastModule } from 'primeng/toast';
+import { ToastService } from './core/services/toast-service/toast.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CustomToastComponent, ToastModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'skillspace-resume-builder';
+
+  constructor(public toastService: ToastService){ }
 }
