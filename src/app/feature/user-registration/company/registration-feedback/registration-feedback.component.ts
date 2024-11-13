462055e5764756758b9f4effb32d8b5408d1a925
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-13 18:10:57 +0000

    feat: add methods and display awaiting, rejected and success status conditionally

--- src/app/feature/user-registration/company/registration-feedback/registration-feedback.component.ts
+++ src/app/feature/user-registration/company/registration-feedback/registration-feedback.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RegistrationFeedbackContentComponent } from '../registration-feedback-content/registration-feedback-content.component';
 
 @Component({
@@ -8,9 +8,54 @@ import { RegistrationFeedbackContentComponent } from '../registration-feedback-c
   templateUrl: './registration-feedback.component.html',
   styleUrl: './registration-feedback.component.scss'
 })
-export class RegistrationFeedbackComponent {
+export class RegistrationFeedbackComponent implements OnInit {
   isAwaitingReview:boolean = false;
   isRejected:boolean = false;
   isSuccessful:boolean = false;
+
+  svgImage!:string;
+  title!:string;
+  description!:string;
+  buttonText!:string;
+
+  ngOnInit():void {
+    this.getFeedbackResponse('success');
+  }
+
+  getFeedbackResponse (status:string) {
+    switch(status) {
+      case 'awaiting':
+        this.handleAwaitingResponse();
+        break;
+      case 'rejected':
+        this.handleRejectedResponse();
+        break;
+      case 'success':
+        this.handleSuccessResponse();
+        break;
+      default: this.handleAwaitingResponse()
+        break;
+    }
+    
+  }
+
+  private handleAwaitingResponse () {
+    this.svgImage = 'review-awaiting';
+    this.title = 'Your account is still under review'
+    this.description = ' Your account will be reviewed by our System Administrator before activation. This process may take 1-3 business days. We will notify you via email once your account has been approved.';
+    this.buttonText = 'Return to home page';
+  }
+  private handleRejectedResponse () {
+    this.svgImage = 'review-rejected';
+    this.title = 'Sorry, your account has been rejected'
+    this.description = ' Your account was reviewed by our System Administrator (1-3 business days). We have notified you via email. Click ‘Sign up’ to restart the process.';
+    this.buttonText = 'Sign up';
+  }
+  private handleSuccessResponse () {
+    this.svgImage = 'review-successful';
+    this.title = 'Congratulation!'
+    this.description = ' Your account will be reviewed by our System Administrator before activation. This process may take 1-3 business days. We will notify you via email once your account has been approved.';
+    this.buttonText = 'Continue to log in';
+  }
 
 }
