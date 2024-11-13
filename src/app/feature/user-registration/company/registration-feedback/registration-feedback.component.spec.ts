fd0895e3c1b7cbff51e5d322f19900db7793601e
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-13 18:28:15 +0000

    feat: add unit test cases

--- src/app/feature/user-registration/company/registration-feedback/registration-feedback.component.spec.ts
+++ src/app/feature/user-registration/company/registration-feedback/registration-feedback.component.spec.ts
@@ -20,4 +20,29 @@ describe('RegistrationFeedbackComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call handleAwaitingResponse when status is "awaiting"', () => {
+    const spy = spyOn(component as any, 'handleAwaitingResponse');
+
+    component.getFeedbackResponse('awaiting');
+
+    expect(spy).toHaveBeenCalled();
+    
+  })
+
+  it('should call handleRejectedResponse when status is "rejected"', () => {
+    const spy = spyOn(component as any, 'handleRejectedResponse');
+
+    component.getFeedbackResponse('rejected');
+    expect(spy).toHaveBeenCalled();
+  })
+
+  it('should call handleSuccessResponse when status is "success"', () => {
+    const spy = spyOn(component as any, 'handleSuccessResponse');
+
+    component.getFeedbackResponse('success');
+    expect(spy).toHaveBeenCalled();
+  })
+  
+  
 });
