94c6d040b22cda7b44dbb32133053b9fa40d94e2
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-13 14:30:26 +0000

    fix: change onPngUpload method name to onFileUpload for clarity

--- src/app/feature/user-registration/company/company-registration/company-registration.component.ts
+++ src/app/feature/user-registration/company/company-registration/company-registration.component.ts
@@ -146,7 +146,7 @@ export class CompanyRegistrationComponent implements OnInit {
     });
   }
 
-  onPngUpload(file: File | null, control:string) {
+  onFileUpload(file: File | null, control:string) {
     console.log('logging file name: ', file?.name);
     if (file) {
       this.companyForm.get(`information.${control}`)?.setValue(file);
