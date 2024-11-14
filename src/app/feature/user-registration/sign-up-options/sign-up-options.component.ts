aff5ce5eef025e7c580325a9d325c9d13df3fc6b
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-14 16:36:11 +0000

    feat: add routing to company sign up page

--- src/app/feature/user-registration/sign-up-options/sign-up-options.component.ts
+++ src/app/feature/user-registration/sign-up-options/sign-up-options.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up-options',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, ],
   templateUrl: './sign-up-options.component.html',
   styleUrl: './sign-up-options.component.scss'
 })
