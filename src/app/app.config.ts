f306f155d771658b0fe87e9f044213c7a8c507ef
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-14 13:50:16 +0000

    feat: provide http client module at the root for the entire application

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,12 +7,14 @@ import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideAnimationsAsync(), 
     provideAnimations(),
+    provideHttpClient(),
     provideStore(), 
     provideEffects(), 
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
