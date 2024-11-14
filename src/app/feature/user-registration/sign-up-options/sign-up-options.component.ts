34320e103704433a574dd41440b1752e2516cc7b
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-14 15:37:24 +0000

    feat: add sign up options

---
+++ src/app/feature/user-registration/sign-up-options/sign-up-options.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up-options',
+  standalone: true,
+  imports: [],
+  templateUrl: './sign-up-options.component.html',
+  styleUrl: './sign-up-options.component.scss'
+})
+export class SignUpOptionsComponent {
+
+}
