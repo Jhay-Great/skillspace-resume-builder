0863a3d33d359f1d6b5ed5397d85c4e184d14616
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-15 07:23:34 +0000

    fix: change method paramter and property values
    Update method parameter types and property value.
    Removes console logs from method.

--- src/app/feature/user-registration/service/user-management.service.ts
+++ src/app/feature/user-registration/service/user-management.service.ts
@@ -1,31 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable, } from '@angular/core';
 import { environment } from '../../../../environments/environment.development';
-import { ICompany } from '../../../core/interfaces/user-managment.interface';
+import { ICompany, ITalent } from '../../../core/interfaces/user-managment.interface';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserRegistrationService {
-  private api:string = environment.userRegistrationApi;
-  private companyEndpoint:string  = '';
-  private talentEndpoint:string  = '';
+  private api:string = environment.baseApi;
+  private companyEndpoint:string  = environment.companyEndpoint;
+  private talentEndpoint:string  = environment.talentEndpoint;
 
   constructor(
     private http:HttpClient,
   ) { }
 
   companySignUp(data:ICompany) {
-    console.log('called service with: ', data)
     return this.submitForApproval(`${this.api}${this.companyEndpoint}`, data);
 
   }
   
-  talentSignUp (data:ICompany) {
+  talentSignUp (data:ITalent) {
     return this.submitForApproval(`${this.api}${this.talentEndpoint}`, data);
   }
   
-  private submitForApproval(api:string, data:ICompany) {
+  private submitForApproval<T>(api:string, data:T) {
     return this.http.post(api, data);
   }
 }
+
