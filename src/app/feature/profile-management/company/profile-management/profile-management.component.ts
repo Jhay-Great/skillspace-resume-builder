244f898a94993f762e77b8b25a9468d2d8c183ba
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-16 13:43:34 +0000

    feat(html, ts): add initial form and input markup and prime ng imports
    Import primeng module in typescript file.
    Use primeng input fields to build form inputs.

--- src/app/feature/profile-management/company/profile-management/profile-management.component.ts
+++ src/app/feature/profile-management/company/profile-management/profile-management.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+// primeng modules
+import { TabViewModule } from 'primeng/tabview';
+import { ButtonModule } from 'primeng/button';
+// import { InputTextModule } from 'primeng/inputtext';
+
+import { InputIconModule } from 'primeng/inputicon';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-profile-management',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    InputTextModule,
+    InputIconModule,
+    IconFieldModule,
+    TabViewModule,
+    ButtonModule,
+  ],
   templateUrl: './profile-management.component.html',
-  styleUrl: './profile-management.component.scss'
+  styleUrl: './profile-management.component.scss',
 })
 export class ProfileManagementComponent {
-
+  description:string = 'This is what applicants will see on your profile.'
 }
