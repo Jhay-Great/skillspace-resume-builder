4215fe8f6b08f94c3bb81c59faa31723ebd0a68d
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-17 18:10:50 +0000

    feat: add markup, style and logic to page-header-description component
    Use @Input decorator to pass content to the component

--- src/app/shared/components/page-header-description/page-header-description.component.ts
+++ src/app/shared/components/page-header-description/page-header-description.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-page-header-description',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './page-header-description.component.scss'
 })
 export class PageHeaderDescriptionComponent {
-
+  @Input() title:string | null = null;
+  @Input() description:string | null = null;
 }
