182df2d6e702517711d1259345a2ccdc6800f858
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-17 12:33:27 +0000

    feat: add initial method for drag and drop logo

--- src/app/feature/profile-management/company/profile-management/profile-management.component.ts
+++ src/app/feature/profile-management/company/profile-management/profile-management.component.ts
@@ -26,4 +26,32 @@ import { InputTextModule } from 'primeng/inputtext';
 })
 export class ProfileManagementComponent {
   description:string = 'This is what applicants will see on your profile.'
+  fileUploaded: FileList | null = null;
+
+  constructor () {};
+
+  onDragOver(event:DragEvent) {
+    this.resetDefaultBrowserSettings(event);
+    const container = event.currentTarget as HTMLElement;
+    container.classList.add('on-drag');
+  };
+  onDragLeave(event:DragEvent) {
+    this.resetDefaultBrowserSettings(event);
+    const container = event.currentTarget as HTMLElement;
+    container.classList.remove('on-drag');
+  };
+  onDrop(event:DragEvent) {
+    this.resetDefaultBrowserSettings(event);
+    console.log('something dropped...')
+    const files = event.dataTransfer?.files;
+    if (files && files.length > 0) {
+      console.log(files);
+      this.fileUploaded = files;
+    }
+  };
+
+  private resetDefaultBrowserSettings (event:Event) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
 }
