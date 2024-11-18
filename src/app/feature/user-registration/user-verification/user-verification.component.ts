b05cb7cabe949b179c8dfe2445b1aadaaa25a700
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-18 11:30:25 +0000

    feat: add otp verification component and method to handle verification

--- src/app/feature/user-registration/user-verification/user-verification.component.ts
+++ src/app/feature/user-registration/user-verification/user-verification.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { OtpVerificationComponent } from "../../../shared/components/otp-verification/otp-verification.component";
+import { UserRegistrationService } from '../service/user-registration.service';
+import { ToastService } from '@src/app/core/services/toast-service/toast.service';
 
 @Component({
   selector: 'app-user-verification',
   standalone: true,
-  imports: [],
+  imports: [OtpVerificationComponent, ],
   templateUrl: './user-verification.component.html',
   styleUrl: './user-verification.component.scss'
 })
 export class UserVerificationComponent {
+
+  constructor (
+    private userRegistration: UserRegistrationService,
+    private toastService: ToastService,
+  ) {};
+  
+  onSubmitOTP(otp:string) {
+    this.userRegistration.verifyOTP(otp);
+    this.toastService.showSuccess('success', 'something');
+
+  }
 
 }
