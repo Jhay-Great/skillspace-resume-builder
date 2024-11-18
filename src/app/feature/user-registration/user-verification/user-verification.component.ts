769cd1c4f568260571702a3eb7e2a2a8970fa93d
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-18 20:20:43 +0000

    feat: add dynamic routing to page based on user
    Dynamically routes to login page is user is a talent and review page is
    user is a company.

--- src/app/feature/user-registration/user-verification/user-verification.component.ts
+++ src/app/feature/user-registration/user-verification/user-verification.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { OtpVerificationComponent } from "../../../shared/components/otp-verification/otp-verification.component";
 import { UserRegistrationService } from '../service/user-registration.service';
 import { ToastService } from '@src/app/core/services/toast-service/toast.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-verification',
@@ -11,16 +12,31 @@ import { ToastService } from '@src/app/core/services/toast-service/toast.service
   styleUrl: './user-verification.component.scss'
 })
 export class UserVerificationComponent {
+  user:string | null = null;
 
   constructor (
     private userRegistration: UserRegistrationService,
     private toastService: ToastService,
+    private router: Router
   ) {};
   
   onSubmitOTP(otp:string) {
     this.userRegistration.verifyOTP(otp);
     this.toastService.showSuccess('success', 'something');
 
+    this.user = this.userRegistration.user()
+
+    if (this.user === 'COMPANY') {
+      console.log('user is a company')
+      this.router.navigate(['auth/review/awaiting']);
+    }
+    if (this.user === 'TALENT') {
+      console.log('user is a talent')
+      this.router.navigate(['auth/login']);
+
+    }
+    console.log(this.user === 'TALENT')
+    console.log(this.userRegistration.user());
   }
 
 }
