a1c02a96eaa7e8974fb8fd7e8f800f3aa090f62d
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-19 07:34:05 +0000

    feat: add drag and drop file input field component

---
+++ src/app/shared/components/drap-n-drop-file-input/drap-n-drop-file-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drap-n-drop-file-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './drap-n-drop-file-input.component.html',
+  styleUrl: './drap-n-drop-file-input.component.scss'
+})
+export class DrapNDropFileInputComponent {
+
+}
