10bf87c7bb33883d1e0b09e91cc1b1e75237a89b
kewablay <kojokewajunior123@gmail.com>
2024-11-19 08:36:46 +0000

    refactor(localStorage): add generic types to local storage service

--- src/app/core/services/localStorageService/local-storage.service.ts
+++ src/app/core/services/localStorageService/local-storage.service.ts
@@ -6,13 +6,13 @@ import { Injectable } from '@angular/core';
 export class LocalStorageService {
   constructor() {}
 
-  setItem(key: string, value: any): void {
+  setItem<T>(key: string, value: T): void {
     localStorage.setItem(key, JSON.stringify(value));
   }
 
-  getItem(key: string): any {
+  getItem<T>(key: string): T | null {
     const data = localStorage.getItem(key);
-    return data ? JSON.parse(data) : null;
+    return data ? (JSON.parse(data) as T) : null;
   }
 
   removeItem(key: string): void {
