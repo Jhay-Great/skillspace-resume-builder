8573e10dc6d6714d6789d423d3d6bdb64a3299d2
kewablay <kojokewajunior123@gmail.com>
2024-11-19 11:37:54 +0000

    refactor(localStorage): add try-catch for JSON.parse and remove empty constructor

--- src/app/core/services/localStorageService/local-storage.service.ts
+++ src/app/core/services/localStorageService/local-storage.service.ts
@@ -4,15 +4,18 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class LocalStorageService {
-  constructor() {}
-
+  
   setItem<T>(key: string, value: T): void {
     localStorage.setItem(key, JSON.stringify(value));
   }
 
   getItem<T>(key: string): T | null {
-    const data = localStorage.getItem(key);
-    return data ? (JSON.parse(data) as T) : null;
+    try {
+      const data = localStorage.getItem(key);
+      return data ? (JSON.parse(data) as T) : null;
+    } catch (error) {
+      return null;
+    }
   }
 
   removeItem(key: string): void {
