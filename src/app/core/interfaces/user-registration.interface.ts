1095be39284c5f9b04095d535cdbe3f42af91794
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-20 09:42:36 +0000

    feat: add response interface data for compnay and talent

--- src/app/core/interfaces/user-registration.interface.ts
+++ src/app/core/interfaces/user-registration.interface.ts
@@ -7,6 +7,15 @@ export interface ICompanyRegistrationDetails  {
     certificate: FormData;
     logo:FormData;
     contact: string;
+
+    // role?: string;
+    // approvalStatus?: string;
+    // isOtpVerified?: boolean;
+}
+export interface ICompanyRegistrationResponse extends ICompanyRegistrationDetails {
+    role: string;
+    approvalStatus: string;
+    isOtpVerified: boolean;
 }
 
 export interface ITalentRegistrationDetails {
@@ -15,6 +24,19 @@ export interface ITalentRegistrationDetails {
     password: string;
     confirmPassword: string;
     contact: string;
+
+    // role?: string;
+    // isOtpVerified?: boolean;
+}
+export interface ITalentRegistrationResponse extends ITalentRegistrationDetails {
+    role: string;
+    isOtpVerified: boolean;
+
 }
 
 export type Status = string;
+
+export interface IOtpData {
+    token: string;
+    email: string;
+}
