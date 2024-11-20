e8ab0669548e2f080d71e0ad93e2bbf296054ab9
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-20 07:23:55 +0000

    feat: add initial admin-approval dashboard
    Includes markup and initial logic

--- src/app/feature/user-registration/admin-approval/admin-dashboard/admin-dashboard.component.ts
+++ src/app/feature/user-registration/admin-approval/admin-dashboard/admin-dashboard.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { IApplicantData } from '@src/app/core/interfaces/user-registration.interface';
+
+// primeng modules
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
+  imports: [TableModule, ButtonModule, TagModule, AvatarModule,  ],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.scss'
 })
 export class AdminDashboardComponent {
+  applicants:IApplicantData[] = [
+    {name: 'amalitech', email: 'ama@gmail.com', status: 'pending', date: '12/03/24'},
+    {name: 'hubtel', email: 'hbt@gmail.com', status: 'approved', date: '12/11/24', image: 'assets/images/Avatar.png'},
+  ];
+
+
+  getSeverity(status: string) {
+    if (status === 'approved') return 'success';
+    if (status === 'rejected') return 'danger';
+    else return 'info'
+  }
 
 }
