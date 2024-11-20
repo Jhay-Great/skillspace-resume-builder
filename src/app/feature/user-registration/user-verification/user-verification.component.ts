e3b6dbc31ccbb95d10990acbc99a71e632f56e03
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-20 10:00:43 +0000

    feat: add user email to opt request data
    Update user data that is sent to the server for the otp verification

--- src/app/feature/user-registration/user-verification/user-verification.component.ts
+++ src/app/feature/user-registration/user-verification/user-verification.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef } from '@angular/core';
 import { OtpVerificationComponent } from "../../../shared/components/otp-verification/otp-verification.component";
 import { UserRegistrationService } from '../service/user-registration.service';
 import { ToastService } from '@src/app/core/services/toast-service/toast.service';
 import { Router } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ITalentRegistrationResponse } from '@src/app/core/interfaces/user-registration.interface';
 
 @Component({
   selector: 'app-user-verification',
@@ -17,22 +19,41 @@ export class UserVerificationComponent {
   constructor (
     private userRegistration: UserRegistrationService,
     private toastService: ToastService,
-    private router: Router
+    private router: Router,
+    private destroyRef: DestroyRef,
   ) {};
   
-  onSubmitOTP(otp:string) {
-    this.userRegistration.verifyOTP(otp);
-    this.toastService.showSuccess('success', 'something');
+  onSubmitOTP(token:string) {
+    const userEmail = this.userRegistration.userEmail();
+    if (!userEmail) return;
+    console.log(userEmail);
 
-    this.user = this.userRegistration.user()
+    const data = { token, email: userEmail };
+    console.log(data);
+    this.userRegistration.verifyOTP(data).pipe(takeUntilDestroyed(this.destroyRef)).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.toastService.showSuccess('success', 'Your verification was successful');
+    
+        this.user = this.userRegistration.user()
+    
+        if (this.user === 'COMPANY') {
+          this.router.navigate(['auth/review/awaiting']);
+        }
+        if (this.user === 'TALENT') {
+          this.router.navigate(['auth/login']);
+    
+        }
 
-    if (this.user === 'COMPANY') {
-      this.router.navigate(['auth/review/awaiting']);
-    }
-    if (this.user === 'TALENT') {
-      this.router.navigate(['auth/login']);
-
-    }
+      },
+      error: error => {
+        console.log('error: ', error);
+        this.toastService.showError('Error', 'The verification code provided is invalid');
+      },
+      complete: () => {
+        console.log('done')
+      }
+    });
   }
 
 }
