77830cd5a7b41f4d9f2c7989727077a07f0d8b42
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-20 17:13:41 +0000

    fix: read data from response data

--- src/app/feature/user-registration/talent/talent-registration/talent-registration.component.ts
+++ src/app/feature/user-registration/talent/talent-registration/talent-registration.component.ts
@@ -76,7 +76,9 @@ export class TalentRegistrationComponent implements OnInit {
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe({
         next: (response) => {
-          const { email, role } = response; 
+          console.log(response);
+          const { email, role } = response.data; 
+          console.log(email, role);
           this.userRegistrationService.userEmail.set(email);
           this.reset();
           this.isLoading = false;
@@ -86,6 +88,7 @@ export class TalentRegistrationComponent implements OnInit {
         error: (error) => {
           this.isLoading = false;
           this.toastService.showError('Error', error.error);
+          console.log('error: ', error);
         },
         complete: () => {
           this.isLoading = false;
