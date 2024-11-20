44add5907cc811a3158d771cecf88c920e21db7c
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-20 09:59:24 +0000

    fix: change request and response types
    Type data sent via http POST and response received

--- src/app/feature/user-registration/service/user-registration.service.ts
+++ src/app/feature/user-registration/service/user-registration.service.ts
@@ -1,10 +1,14 @@
-import { Injectable, signal, } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '@src/environments/environment.development';
 import {
   ICompanyRegistrationDetails,
+  ICompanyRegistrationResponse,
+  IOtpData,
   ITalentRegistrationDetails,
+  ITalentRegistrationResponse,
 } from '@src/app/core/interfaces/user-registration.interface';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -14,29 +18,46 @@ export class UserRegistrationService {
   private companyEndpoint: string = environment.COMPANY_ENDPOINT;
   private talentEndpoint: string = environment.TALENT_ENDPOINT;
   private otpEndpoint: string = environment.OTP_ENDPOINT;
+  private otpBase = environment.OTP_BASE_API;
 
   // signals
-  otpResponse = signal<string | null> (null);
+  otpResponse = signal<string | null>(null);
   user = signal<string | null>(null);
+  userEmail = signal<string | null>(null);
 
   constructor(private http: HttpClient) {}
 
-  companySignUp(data: FormData) {
-    return this.submitForApproval(`${this.api}${this.companyEndpoint}`, data);
+  private submitForApproval<D, T>(api: string, data: D) {
+    console.log('api: ', api);
+    console.log('data: ', data);
+    return this.http.post<T>(api, data);
   }
 
-  talentSignUp(data: ITalentRegistrationDetails) {
-    return this.submitForApproval(`${this.api}${this.talentEndpoint}`, data);
+  companySignUp(data: FormData): Observable<ICompanyRegistrationResponse> {
+    return this.submitForApproval<FormData, ICompanyRegistrationResponse>(
+      `${this.api}/${this.companyEndpoint}`,
+      data
+    );
   }
 
-  private submitForApproval<T>(api: string, data: T) {
-    return this.http.post(api, data);
+  talentSignUp(
+    data: ITalentRegistrationDetails
+  ): Observable<ITalentRegistrationResponse> {
+    return this.submitForApproval<
+      ITalentRegistrationDetails,
+      ITalentRegistrationResponse
+    >(`${this.api}/${this.talentEndpoint}`, data);
   }
 
-  verifyOTP(otp: string) {
-    return this.submitForApproval(
-      `${this.api}${this.otpEndpoint}`,
+  verifyOTP(otp: IOtpData) {
+    return this.submitForApproval<IOtpData, null>(
+      `${this.otpBase}/${this.otpEndpoint}`,
       otp
-    )
+    ).pipe(
+      map(data => {
+        console.log(data);
+        return data;
+      })
+    );
   }
 }
