f260a79165d1564a823f23dff67fb7aae0ccafe9
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-20 15:26:30 +0000

    feat: add company profile page on admin profile

--- src/app/feature/user-registration/admin-approval/pages/selected-company-profile/selected-company-profile.component.ts
+++ src/app/feature/user-registration/admin-approval/pages/selected-company-profile/selected-company-profile.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
 
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ButtonModule } from 'primeng/button';
+import { ConfirmationService } from 'primeng/api';
+import { TooltipModule } from 'primeng/tooltip';
+
 @Component({
   selector: 'app-selected-company-profile',
   standalone: true,
-  imports: [],
+  imports: [ConfirmDialogModule, ButtonModule, TooltipModule ],
   templateUrl: './selected-company-profile.component.html',
-  styleUrl: './selected-company-profile.component.scss'
+  styleUrl: './selected-company-profile.component.scss',
 })
 export class SelectedCompanyProfileComponent {
+  constructor(private confirmationService: ConfirmationService) {}
 
+  confirm() {
+    this.confirmationService.confirm({
+      header: 'Accept company',
+      message:
+        'Are you sure that you want to accept company? This action cannot be reversed.',
+      accept: () => {
+
+      },
+      reject: () => {
+        
+      },
+    });
+  }
+
+  reject() {
+    this.confirmationService.confirm({
+      header: 'Accept company',
+      message:
+        'Are you sure that you want to accept company? This action cannot be reversed.',
+      accept: () => {
+
+      },
+      reject: () => {
+
+      },
+    });
+  }
 }
