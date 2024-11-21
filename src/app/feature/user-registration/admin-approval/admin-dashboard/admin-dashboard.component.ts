788bfbfecd948fd63cbfc9ba201c904b7c583937
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-21 00:07:26 +0000

    feat: get all registered companies data from server

--- src/app/feature/user-registration/admin-approval/admin-dashboard/admin-dashboard.component.ts
+++ src/app/feature/user-registration/admin-approval/admin-dashboard/admin-dashboard.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { IApplicantData } from '@src/app/core/interfaces/user-registration.interface';
+import { Component, DestroyRef } from '@angular/core';
+import { ApplicantResponse, ApplicantsData, IApplicantData } from '@src/app/core/interfaces/user-registration.interface';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { AdminApprovalService } from '../../service/admin-approval/admin-approval.service';
 
 // primeng modules
 import { TableModule } from 'primeng/table';
@@ -7,25 +9,50 @@ import { ButtonModule } from 'primeng/button';
 import { TagModule } from 'primeng/tag';
 import { AvatarModule } from 'primeng/avatar';
 import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { ToastService } from '@src/app/core/services/toast-service/toast.service';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [TableModule, ButtonModule, TagModule, AvatarModule, OverlayPanelModule, ],
+  imports: [
+    TableModule,
+    ButtonModule,
+    TagModule,
+    AvatarModule,
+    OverlayPanelModule,
+  ],
   templateUrl: './admin-dashboard.component.html',
-  styleUrl: './admin-dashboard.component.scss'
+  styleUrl: './admin-dashboard.component.scss',
 })
 export class AdminDashboardComponent {
-  applicants:IApplicantData[] = [
-    {name: 'amalitech', email: 'ama@gmail.com', status: 'pending', date: '12/03/24'},
-    {name: 'hubtel', email: 'hbt@gmail.com', status: 'approved', date: '12/11/24', image: 'assets/images/Avatar.png'},
-  ];
+  applicants!: ApplicantsData[];
 
+  constructor(
+    private adminApprovalService: AdminApprovalService,
+    private destroyRef: DestroyRef,
+    private toastService: ToastService,
+  ) {}
+
+  ngOnInit(): void {
+    this.adminApprovalService
+      .getCompanies()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: response => {
+          console.log(response.data);
+          this.applicants = response.data;
+
+        },
+        error: error => {
+          this.toastService.showError('Error', 'Failed to load data')
+        },
+        complete: () => {},
+      });
+  }
 
   getSeverity(status: string) {
     if (status === 'approved') return 'success';
     if (status === 'rejected') return 'danger';
-    else return 'info'
+    else return 'info';
   }
-
 }
