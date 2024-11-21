67931099d41af9464f1fc29d7674a28e6def8e9e
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-21 07:49:41 +0000

    fix: udpdate approval api string

--- src/app/feature/user-registration/service/admin-approval/admin-approval.service.ts
+++ src/app/feature/user-registration/service/admin-approval/admin-approval.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { ApplicantResponse } from '@src/app/core/interfaces/user-registration.interface';
+import { Injectable, signal } from '@angular/core';
+import { ApplicantResponse, ApplicantsData } from '@src/app/core/interfaces/user-registration.interface';
 import { environment } from '@src/environments/environment.development';
 import { Observable } from 'rxjs';
 
@@ -10,6 +10,10 @@ import { Observable } from 'rxjs';
 export class AdminApprovalService {
   api:string = environment.BASE_API;
   companies:string = environment.ALL_COMPANIES;
+  approval:string = environment.APPROVAL_ENDPOINT;
+  // rejected:string = environment.REJECTED;
+
+  selectedUser = signal<ApplicantsData | null>(null);
 
   constructor(
     private http: HttpClient,
@@ -17,5 +21,18 @@ export class AdminApprovalService {
 
   getCompanies():Observable<ApplicantResponse> {
     return this.http.get<ApplicantResponse>(`${this.api}/${this.companies}`);
+  }
+
+  post<D, T>(api:string, data: D) {
+    console.log('api: ', api);
+    return this.http.post<T>(api, data);
+  }
+
+  acceptApplicant(id:number) {
+    return this.post(`${this.api}/${this.approval}/${id}/approve`, id);
+  }
+
+  rejectApplicant(id:number) {
+    return this.post(`${this.api}/${this.approval}/${id}/reject`, id);
   }
 }
