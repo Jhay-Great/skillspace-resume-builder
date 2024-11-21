66e8882cabe34cc80673a5a68a58b178e11a7609
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-21 00:10:31 +0000

    feat: add admin-approval service and fetch all registered companies from there server

--- src/app/feature/user-registration/service/admin-approval/admin-approval.service.ts
+++ src/app/feature/user-registration/service/admin-approval/admin-approval.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ApplicantResponse } from '@src/app/core/interfaces/user-registration.interface';
+import { environment } from '@src/environments/environment.development';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminApprovalService {
+  api:string = environment.BASE_API;
+  companies:string = environment.ALL_COMPANIES;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getCompanies():Observable<ApplicantResponse> {
+    return this.http.get<ApplicantResponse>(`${this.api}/${this.companies}`);
+  }
 }
