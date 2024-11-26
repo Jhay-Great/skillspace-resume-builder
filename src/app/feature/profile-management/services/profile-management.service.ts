f00a4233ea3c25cc3a15b7ff1b51b84de886038d
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-26 11:56:46 +0000

    feat: inject http client module and add update method to make put request

--- src/app/feature/profile-management/services/profile-management.service.ts
+++ src/app/feature/profile-management/services/profile-management.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '@src/environments/environment.development';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileManagementService {
+  BASE_API:string = environment.BASE_API;
+  COMPANY_PROFILE_ENDPOINT:string = environment.COMPANY_PROFILE_ENDPOINT
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  // private method to make the PUT request
+  private update<D, T> (api:string, data:D):Observable<T> {
+    return this.http.put<T>(api, data);
+  }
+
+  // UPDATE company profile
+  // updateCompanyProfile(data:any) {
+  //   this.update(`${this.BASE_API}/${this.COMPANY_PROFILE_ENDPOINT}/`)
+  // }
 }
