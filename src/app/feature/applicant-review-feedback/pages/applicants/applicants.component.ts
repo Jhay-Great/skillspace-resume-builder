2840e16f512fe786388a8f2283c229a629dd6b69
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-27 15:39:05 +0000

    feat: add initial component for applicants and applicant profile

---
+++ src/app/feature/applicant-review-feedback/pages/applicants/applicants.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-applicants',
+  standalone: true,
+  imports: [],
+  templateUrl: './applicants.component.html',
+  styleUrl: './applicants.component.scss'
+})
+export class ApplicantsComponent {
+
+}
