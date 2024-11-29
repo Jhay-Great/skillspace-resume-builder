88fd3c7715fe3fe592bc233dbdab35865dbb261d
Jhay-Great <alemonai.jenef@gmail.com>
2024-11-29 15:19:23 +0000

    fix: remove comments and console log

--- src/app/shared/components/drap-n-drop-file-input/drap-n-drop-file-input.component.ts
+++ src/app/shared/components/drap-n-drop-file-input/drap-n-drop-file-input.component.ts
@@ -11,7 +11,6 @@ import { ToastService } from '@src/app/core/services/toast-service/toast.service
 export class DrapNDropFileInputComponent {
   description:string = 'This is what applicants will see on your profile.'
   fileUploaded: File | null = null;
-  // fileUploaded: FileList | null = null;
   previewImage:string | null = null;
 
   @Input () label:string | null = null;
@@ -44,8 +43,6 @@ export class DrapNDropFileInputComponent {
 
     const files = event.dataTransfer?.files;
     if (files && files.length > 0) {
-      // this.fileUploaded = files;
-      console.log(this.fileUploaded);
       this.handleFile(files[0]);
     }
   };
@@ -63,7 +60,6 @@ export class DrapNDropFileInputComponent {
       // handle error response or feedback here
       this.toastService.showError('Failed to upload', 'Incompatible file uploaded');
       this.uploadedFile.emit(null);
-      console.log('Please upload only image files.');
     }
   }
 
