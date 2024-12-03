b883cb5a338d29b0b43e472c3f6eaa259b465e91
Pe-KaY <peekaylink@gmail.com>
2024-12-03 14:58:31 +0000

    refactor: use tag component for status and update setting icon

--- src/app/shared/components/programme-card/programme-card.component.ts
+++ src/app/shared/components/programme-card/programme-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { TagComponent } from '../tag/tag.component';
 
 @Component({
   selector: 'app-programme-card',
   standalone: true,
-  imports: [OverlayPanelModule],
+  imports: [OverlayPanelModule, TagComponent],
   templateUrl: './programme-card.component.html',
   styleUrl: './programme-card.component.scss',
 })
